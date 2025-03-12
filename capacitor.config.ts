5b3b27f34c7d0beea138b777f02db5e24b5897eb
sazid <sazid@qboid.io>
2025-03-12 21:53:47 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Soni_Sync_player',
+  webDir: 'www'
+};
+
+export default config;
